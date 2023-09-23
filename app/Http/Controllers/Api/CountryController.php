c9caf08febd16b70ea08ca5f44e91989ed3b5157
Taha khalid <tth31770@gmail.com>
2023-09-24 01:45:09 +0300

    first commit

--- app/Http/Controllers/Api/CountryController.php
+++ app/Http/Controllers/Api/CountryController.php
@@ -3,30 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\SubMassgTest;
 use App\Models\Country;
 use Illuminate\Http\Request;
 
 class CountryController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function __invoke(Request $request)
     {
-        try {
-         $countries = Country::all();
-
-           return response()->json([
-                'status_code' => 200,
-                'message' => 'Success',               
-                'countries' =>$countries ,
-
-            ], 200);
-        } catch (\Throwable $e) {
-             return response()->json(['message' => 'Failed to retrieve countries.', 'status_code' => 500], 500);
+        for ($i=0; $i < 5; $i++) { 
+            dispatch(new SubMassgTest($i));
         }
     }
 }
